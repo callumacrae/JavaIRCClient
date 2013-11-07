02a1f3cfd3acf077776ffc9be3d944424991e059
callumacrae <callum@lynxphp.com>
2013-11-07 00:46:28 +0000

    Rewrote README, cleaned up imports, added blank line to end of file.

---
+++ README.md
@@ -0,0 +1,5 @@
+# callumacrae/JavaIRCClient
+
+My first proper project in Java, a simple IRC client.
+
+Coded in IntelliJ CE.
--- src/IRC.java
+++ src/IRC.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
