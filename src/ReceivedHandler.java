46688eb7de6b7ca3494141508c31157f40de1ad7
callumacrae <callum@lynxphp.com>
2013-11-10 22:21:06 +0000

    Added comments to EventListener.

--- src/ReceivedHandler.java
+++ src/ReceivedHandler.java
@@ -23,16 +23,29 @@ public class ReceivedHandler implements EventListener {
 		this.frame = frame;
 	}
 
+	/**
+	 * Fired when a connection is established and user is connected.
+	 *
+	 * @param client Client object that has been connected to.
+	 */
 	@Override
 	public void connected(Client client) {
 		System.out.println("Connected!");
 		this.client = client;
 	}
 
+	/**
+	 * Fired when the client disconnects.
+	 */
 	@Override
 	public void disconnected() {
 	}
 
+	/**
+	 * Fired when a line of text is sent by us to the server.
+	 *
+	 * @param line The line that was sent.
+	 */
 	@Override
 	public void lineSent(String line) {
 //		System.out.println("Sent: " + line);
@@ -40,6 +53,11 @@ public class ReceivedHandler implements EventListener {
 		console.addElement(line);
 	}
 
+	/**
+	 * Fired when a line of text is received from the server.
+	 *
+	 * @param line The line that was received.
+	 */
 	@Override
 	public void lineReceived(String line) {
 //		System.out.println("Received: " + line);
@@ -47,16 +65,34 @@ public class ReceivedHandler implements EventListener {
 		console.addElement(line);
 	}
 
+	/**
+	 * Fired when a PRIVMSG is received from a channel.
+	 *
+	 * @param channel String containing channel name.
+	 * @param user User object of sender.
+	 * @param message The message.
+	 */
 	@Override
 	public void messageReceived(String channel, User user, String message) {
 		content.get(channel).addElement(String.format("<%s> %s", user.nick, message));
 	}
 
+	/**
+	 * Fired when a message is send to the server. Useful for logging.
+	 *
+	 * @param destination String of nick or channel name.
+	 * @param message The message.
+	 */
 	@Override
 	public void messageSent(String destination, String message) {
 		content.get(destination).addElement(String.format("<%s> %s", client.getNick(), message));
 	}
 
+	/**
+	 * Fired when a PRIVMSG is received from a user.
+	 * @param user User object of sender.
+	 * @param message The message.
+	 */
 	@Override
 	public void queryReceived(User user, String message) {
 		DefaultListModel query;
@@ -73,6 +109,12 @@ public class ReceivedHandler implements EventListener {
 		user.switchTo();
 	}
 
+	/**
+	 * Fired when a channel is joined (when the server sends the join stuff,
+	 * not when the user types /join).
+	 *
+	 * @param channel Channel object representing the channel connected to.
+	 */
 	@Override
 	public void channelJoined(Channel channel) {
 		channels.addElement(channel.name);
@@ -80,11 +122,25 @@ public class ReceivedHandler implements EventListener {
 		channel.switchTo();
 	}
 
+	/**
+	 * Fired when the user switches channel. This shouldn't really be in the IRC package.
+	 * @param channel String containing channel name.
+	 */
 	@Override
 	public void channelSwitched(String channel) {
 		contentJList.setModel(content.get(channel));
 	}
 
+	/**
+	 * Fired when a nick is changed (either ours or someone else's). It is
+	 * called before the nick is changed, but this behaviour should not be
+	 * relied upon - user oldnick and newnick, not user.nick.
+	 *
+	 * @param user User object of the user whose nick changed.
+	 * @param oldnick The old nick.
+	 * @param newnick The nick that the user changed to.
+	 * @param us True if us.
+	 */
 	@Override
 	public void nickChanged(User user, String oldnick, String newnick, boolean us) {
 
--- src/irc/EventListener.java
+++ src/irc/EventListener.java
@@ -8,18 +8,80 @@ import irc.communicator.*;
  * Not all methods are called yet, WIP.
  */
 public interface EventListener {
-	public void connected(Client connection);
+	/**
+	 * Fired when a connection is established and user is connected.
+	 *
+	 * @param client Client object that has been connected to.
+	 */
+	public void connected(Client client);
+
+	/**
+	 * Fired when the client disconnects.
+	 */
 	public void disconnected();
 
+	/**
+	 * Fired when a line of text is sent by us to the server.
+	 *
+	 * @param line The line that was sent.
+	 */
 	public void lineSent(String line);
+
+	/**
+	 * Fired when a line of text is received from the server.
+	 *
+	 * @param line The line that was received.
+	 */
 	public void lineReceived(String line);
 
+	/**
+	 * Fired when a channel is joined (when the server sends the join stuff,
+	 * not when the user types /join).
+	 *
+	 * @param channel Channel object representing the channel connected to.
+	 */
 	public void channelJoined(Channel channel);
+
+	/**
+	 * Fired when the user switches channel. This shouldn't really be in the IRC package.
+	 * @param channel String containing channel name.
+	 */
+	// @todo: Move out of IRC package
 	public void channelSwitched(String channel);
+
+	/**
+	 * Fired when a PRIVMSG is received from a channel.
+	 *
+	 * @param channel String containing channel name.
+	 * @param user User object of sender.
+	 * @param message The message.
+	 */
 	public void messageReceived(String channel, User user, String message);
+
+	/**
+	 * Fired when a message is send to the server. Useful for logging.
+	 *
+	 * @param destination String of nick or channel name.
+	 * @param message The message.
+	 */
 	public void messageSent(String destination, String message);
+
+	/**
+	 * Fired when a PRIVMSG is received from a user.
+	 * @param user User object of sender.
+	 * @param message The message.
+	 */
 	public void queryReceived(User user, String message);
 
-	// This is called BEFORE the users' nick is changed. Use oldnick and newnick, not user.nick.
+	/**
+	 * Fired when a nick is changed (either ours or someone else's). It is
+	 * called before the nick is changed, but this behaviour should not be
+	 * relied upon - user oldnick and newnick, not user.nick.
+	 *
+	 * @param user User object of the user whose nick changed.
+	 * @param oldnick The old nick.
+	 * @param newnick The nick that the user changed to.
+	 * @param us True if us.
+	 */
 	public void nickChanged(User user, String oldnick, String newnick, boolean us);
 }
